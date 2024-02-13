0f1762833e6ced7e3b8b257623b92f62be20341c
lsfullstack <lsfullstack@gmail.com>
2024-02-13 16:04:19 -0300

    feat: adds transaction creation route

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,11 @@
 import fastify from 'fastify'
-import { knex } from './database'
-import { randomUUID } from 'node:crypto'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.post('/transactions', async () => {
-  const transaction = await knex('transactions')
-    .insert({
-      id: randomUUID(),
-      title: 'Transação de teste',
-      amount: 1000,
-    })
-    .returning('*')
-
-  return transaction
+app.register(transactionsRoutes, {
+  prefix: '/transactions'
 })
 
 app
