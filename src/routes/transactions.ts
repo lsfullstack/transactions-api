781384e3ee207be69ced2ff5e2b82490a831f5dc
lsfullstack <lsfullstack@gmail.com>
2024-02-13 16:10:40 -0300

    feat: adds list and retrieve transation routes

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,24 @@ import { knex } from '../database'
 import { randomUUID } from 'node:crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTrasactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTrasactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return { transaction }
+  })
+
   app.post('/', async (request, reply) => {
     
     const createTransactionBodySchema = z.object({
