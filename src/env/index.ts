5daea2fba1005e2985d6e6cd095973b495da677f
lsfullstack <lsfullstack@gmail.com>
2024-02-13 21:31:54 -0300

    fix: port env

--- src/env/index.ts
+++ src/env/index.ts
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === 'test') {
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
-  PORT: z.string().default('3333'),
+  PORT: z.coerce.string().default('3333'),
   DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(),
 })
